73f9b1748ad3cdb5b4bea013e707765534d6775a
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-13 19:27:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@
 import React from "react";
 import './../styles/App.css';
 
-const App = () => {
+function Tooltip({ text, children }) {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="tooltip">
+      {children}
+      <span className="tooltiptext">{text}</span>
     </div>
   )
 }
 
-export default App
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <Tooltip text="This is a tooltip">
+        <h3>Hover over me</h3>
+      </Tooltip>
+
+      <br />
+
+      <Tooltip text="Another tooltip example">
+        <p>Hover over me to see another tooltip</p>
+      </Tooltip>
+    </div>
+  );
+}
+
+export default App;

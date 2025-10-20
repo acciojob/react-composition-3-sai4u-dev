6c162fe423a3e803374281d8b775770484d97e91
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-20 02:43:15 +0000

    up

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -2,28 +2,31 @@
 import React, { useState } from 'react';
 
 const Tooltip = ({ text, children }) => {
-    const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-    const show = () => setVisible(true);
-    const hide = () => setVisible(false);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
 
-    return (
-        <div
-            className="tooltip"
-            onMouseEnter={show}
-            onMouseLeave={hide}
-            onFocus={show}
-            onBlur={hide}
-            tabIndex={0} // make it focusable for accessibility
-        >
-            {children}
-            {visible && (
-                <div className="tooltiptext" role="tooltip" aria-hidden={!visible}>
-                    {text}
-                </div>
-            )}
-        </div>
-    );
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={show}
+      onMouseLeave={hide}
+      onFocus={show}
+      onBlur={hide}
+      tabIndex={0}
+    >
+      {children}
+      <div
+        className="tooltiptext"
+        role="tooltip"
+        aria-hidden={!visible}
+        style={{ display: visible ? 'block' : 'none' }}
+      >
+        {text}
+      </div>
+    </div>
+  );
 };
 
 export default Tooltip;
